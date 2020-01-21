b9cda5ee7bc49e48cfdc9f0e3d25a1c8605c01e2
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-01-21 13:59:31 -0500

    added basic layout for dashboard

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'form';
+
+export default class Dashboard extends Component(){
+    render(){
+        return(
+            <div className='container dashboard'>
+                <div className='row'>
+                    <div>className='col s12 m6</div>
+                    <div>className='col s12 m5</div>
+                </div>
+            </div>
+        )
+    }
+}
