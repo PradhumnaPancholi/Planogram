c24ec59b66251990b6fb408eae478421415a4af2
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-01-21 13:41:03 -0500

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar.component';
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className='App'></div>
+      <Navbar />
+    </BrowserRouter>
   );
 }
 
