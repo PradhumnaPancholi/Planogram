694eb660a5fa73d9677f06dee3a8acf952597852
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-01-21 13:47:25 -0500

    added signedinoptions

--- src/components/layout/Navbar.component.js
+++ src/components/layout/Navbar.component.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SignedInOptions from './SignedInOptions';
+
 const Navbar = () => {
     return(
         <nav>   
             <div className='nav-wrapper'>
                 <Link to='/' className='brand-logo'>Planogram</Link>
+                <SignedInOptions />
             </div>
         </nav>
     )
---
+++ src/components/layout/SignedInOptions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignedInOptions = () => {
+    return(
+        <ul className='right'>
+            <li><NavLink to='/'>New Project</NavLink></li>
+            <li><NavLink to='/'>Logout</NavLink></li>
+            <li><NavLink to='/' className='btn btn-floating white'>U</NavLink></li>
+        </ul>
+    )
+}
+
+export default SignedInOptions
