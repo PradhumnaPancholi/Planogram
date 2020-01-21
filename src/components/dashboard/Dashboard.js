9e6cdf44c969785a671496c78ec1590640c2f158
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-01-21 14:06:24 -0500

    component setup for notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'form';
 
+import Notifications from './Notifications';
+
 export default class Dashboard extends Component(){
     render(){
         return(
             <div className='container dashboard'>
                 <div className='row'>
-                    <div>className='col s12 m6</div>
-                    <div>className='col s12 m5</div>
+                    <div className='col s12 m6'></div>
+                    <div className='col s12 m5'>
+                        <Notifications />
+                    </div>
                 </div>
             </div>
         )
