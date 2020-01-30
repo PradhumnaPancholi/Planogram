f6bfe9a166b34d4c33cdf8b4b3cb4447c353af21
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-01-29 19:51:03 -0500

    added cndotional rendering for navbar with respect to auth status, Note:- works as expected

--- src/components/layout/Navbar.component.js
+++ src/components/layout/Navbar.component.js
@@ -5,13 +5,21 @@ import { Link } from 'react-router-dom';
 import SignedInOptions from './SignedInOptions';
 import SignedOutOptions from './SignedOutOptions';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    console.log(props)
+    //logic to verify auth status and show links accordingly//
+    let visibleLinks
+    if(props.auth.uid){
+        visibleLinks = <SignedInOptions />
+    }else{
+        visibleLinks = <SignedOutOptions />
+    }
+
     return(
         <nav>   
             <div className='nav-wrapper'>
                 <Link to='/' className='brand-logo'>Planogram</Link>
-                <SignedInOptions />
-                <SignedOutOptions />
+                {visibleLinks}
             </div>
         </nav>
     )
@@ -19,7 +27,7 @@ const Navbar = () => {
 
 const mapStateToProps = (state) => {
     return{
-
+        auth: state.firebase.auth
     }
 }
  export default connect(mapStateToProps) (Navbar)
