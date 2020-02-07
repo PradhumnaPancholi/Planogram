abd40490c5b1f03d8c07280faf91253bf8a4e000
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-02-06 22:58:31 -0500

    added cloud function to trigger on project creation for notifications

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const firebaseAdmin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+firebaseAdmin.initializeApp(functions.config().firebase)
+
+//function to add notication to firestore collection
+const createNotification = (notification) => {
+    return firebaseAdmin.firestore().collection('notifications').add(notification)
+    .then((notification) => console.log('Added notification', notification))
+}
+// function to trigger a notificaton when a new project is created//
+exports.projectCreated = functions.firestore.document('projects/{projectId}').onCreate((doc) => {
+
+    //get project data from doc in response//
+    let project = doc.data()
+    //create a new notification object//
+    let notification = {
+        content: 'added a new project',
+        user: `${project.authorFirstName} ${project.authorLastName}`,
+        time: firebaseAdmin.firestore.FieldValue.serverTimestamp()
+    }
+
+    //pass notifcation to create a new document in collection//
+    return createNotification(notification)
+})
+
+// dummy function
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
