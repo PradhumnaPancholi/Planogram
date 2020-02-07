30cd2d674b02639bd0f33ced0327081cbae67e9a
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-02-06 22:22:17 -0500

    layed out basic notification component with dummy data, note:- no styling

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Notifications = () => {
     return (
-        <div>
-            <h3>Notification</h3>
+        <div className='notification-center section'>
+            <div className='card z-depth-0'>
+                <div className='card-content'>
+                    <div className='card-title'> Notifications</div>
+                    <ul className='notifications'>
+                        <li>Notification 1</li>
+                        <li>Notification 2</li>
+                        <li>Notification 3</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
