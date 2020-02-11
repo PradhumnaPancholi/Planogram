e77906015e43da7d7c004f8496814a443b8e61b9
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2020-02-11 00:46:40 -0500

    setup dist folder for deployment

---
+++ dist/precache-manifest.2e02028c487c0ebfe40d29c2ace97e0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31e62ed4ebd5e22edb6eddabfd3cd305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78782518d6aff4cd7e33",
+    "url": "/static/css/main.e2ae2353.chunk.css"
+  },
+  {
+    "revision": "e7f88778b656d1fd33a1",
+    "url": "/static/js/2.81edbfbd.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/static/js/2.81edbfbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78782518d6aff4cd7e33",
+    "url": "/static/js/main.610a5ffe.chunk.js"
+  },
+  {
+    "revision": "04fa8bc8f816e68283f4",
+    "url": "/static/js/runtime-main.74c84e63.js"
+  }
+]);
